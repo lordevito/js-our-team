dab032a491e5949452beace4460b4ea8d23c3431
lordevito <lollo.devito@hotmail.com>
2023-11-20 20:08:00 +0100

    feat: creato array di oggetti

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Esercizio Our Team
+
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+
+//Creiamo l'array di oggetti coi dati in nostro possesso
--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
 `use strict`;
+
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+
+//Creiamo l'array di oggetti coi dati in nostro possesso
+const staff = [{
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruolo: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg",
+
+    },
+
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruolo: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+
+    },
+
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruolo: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+
+    },
+
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        ruolo: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+
+    },
+
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruolo: "Developer",
+        img: "scott-estrada-developer.jpg",
+
+    },
+
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruolo: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+
+    },
+]
