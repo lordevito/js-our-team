5bad4051c725ff36da0567063f6d1f995f30467d
lordevito <lollo.devito@hotmail.com>
2023-11-20 22:01:38 +0100

    feat: bonus 1-2

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
 
 //Creiamo l'array di oggetti coi dati in nostro possesso
+function generateTemplate() {
 const staff = [{
         nome: "Wayne",
         cognome: "Barnett",
@@ -55,10 +56,49 @@ const staff = [{
     },
 ];
 
+
+
+
+const container = document.getElementById("maincontent");
+const row = document.createElement("div");
+const col = document.createElement("div");
+const card = document.createElement("div");
+const img = document.createElement("img");
+const cardBody = document.createElement("div");
+const cardTitle = document.createElement("h5");
+const cardText = document.createElement("p");
+col.appendChild(card);
+card.appendChild(img);
+card.appendChild(cardBody);
+cardBody.appendChild(cardTitle);
+cardBody.appendChild(cardText);
+row.classList.add("row");
+col.classList.add("col-12", "col-lg-4", "col-md-6", "d-flex", "justify-content-center", "pb-3");
+card.classList.add("card");
+img.classList.add("card-img-top");
+cardBody.classList.add("card-body");
+cardTitle.classList.add("card-title", "fs-4");
+cardText.classList.add("card-text");
+
+for (i = 0; i < staff.length; i++) {
+    const member = staff[i];
+    cardTitle.textContent = `${member.nome + " "}`
+    cardText.textContent = `${member.ruolo}`;
+    img.src = `img/${member.img}`;
+    row.appendChild(col.cloneNode(true));
+}
+
+container.appendChild(row);
+
+}
+
+generateTemplate();
+
 //Creo un ciclo per stampare in console il nome, ruolo e la stringa della foto di ognuno dei componenti dello staff
 
-for (let i = 0; i < staff.length; i++) {
-    console.log(staff[i].nome);
-    console.log(staff[i].ruolo);
-    console.log(staff[i].img);
-}
+
+// for (let i = 0; i < staff.length; i++) {
+//     console.log(staff[i].nome);
+//     console.log(staff[i].ruolo);
+//     console.log(staff[i].img);
+// };
